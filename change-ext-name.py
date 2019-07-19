cb811c4d2b65663a97c6313393a61326308cdc7a
sunson1980 <39400311+sunson1980@users.noreply.github.com>
2019-07-19 11:56:27 +0800

    Update change-ext-name.py

--- change-ext-name.py
+++ change-ext-name.py
@@ -1,1 +1,8 @@
-print("aa")
+'''
+ # @ Author: chenbo
+ # @ Create Time: 2019-07-18 12:25:53
+ # @ Modified by: chenbo
+ # @ Modified time: 2019-07-19 11:48:39
+ # @ Description:This will batch rename a group of files in a given directory,
+once you pass the current and new extensions
+ '''
