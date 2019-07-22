fb571c2f636cae5d620f53ca6685773abf38023a
sunson1980 <39400311+sunson1980@users.noreply.github.com>
2019-07-22 12:35:42 +0800

    gg

--- creat_dir_if_not_there.py
+++ creat_dir_if_not_there.py
@@ -6,3 +6,47 @@
  # @ Description:creat new dir
  '''
 import os
+import argparse
+
+
+def creat_dir(dirc, newdir):
+    '''
+    creat a directory if it is not exists
+    @parameter dirc:where to creat a directory
+    @parameter newdir:the new directory
+    '''
+    try:
+        if os.path.isdir(dirc):
+            if not os.path.exists(os.path.join(dirc, newdir)):
+                os.makedirs(os.path.join(dirc, newdir))
+            else:
+                print("The directory already exists")
+    except Exception as event:
+        print(event)
+
+
+def get_parser():
+    """
+    def arguments
+    """
+    parser = argparse.ArgumentParser(
+        description='creat a new directory if it don`t exists')
+    parser.add_argument('dirc', type=str, nargs=1, help='the path')
+    parser.add_argument('newdir', type=str, nargs=1, help='new directory')
+    return parser
+
+
+def main():
+    """
+    This will be called if the script is directly invoked.
+    """
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    dirc = args['dirc'][0]
+    newdir = args['newdir'][0]
+    creat_dir(dirc, newdir)
+
+
+if __name__ == '__main__':
+    # creat_dir(r'C:\Users\陈波\Downloads\pyttest', 'newdir')
+    main()
